0c3af577d006930fe6d143407f1f0938acc3c236
Broc <thebattlefrog@gmail.com>
2018-02-17 18:36:04 -0500

    Added Sounds and Music
    It only took me WAY TOO LONG to find out.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -15,7 +15,12 @@ var StartPos
 func _ready():
 	StartPos = get_transform()
 	ScreenSize = get_viewport_rect().size
-
+	
+	$WallHitSFX.stream.loop = false
+	$RespawnSFX.stream.loop = false
+	$LevelCompleteSFX.stream.loop = false
+	$PointHitSFX.stream.loop = false
+	
 func _process(delta):
 	Velocity = Vector2()
 	
@@ -44,16 +49,29 @@ func CheckForCollisions(collisions):
 	if collisions:
 		print("Collision: ", collisions.collider.name)
 		if collisions.collider.name == "EndBlock":
-			# if EndBlockRef.PointCollected:
+			$LevelCompleteSFX.play()
+			# Wait for some time...
 			EndBlockRef.OnPlayerEndBlockHit("res://Scenes/MainMenu.tscn")
 		elif collisions.collider.name == "Blocks":
+			$WallHitSFX.play()
 			restart()
 		elif collisions.collider.name == "Point":
-			# emit_signal("PointCollected")
+			$PointHitSFX.play()
 			pass
 	
 func restart():
+	$RespawnSFX.play()
 	# This is the correct way to get the X and Y coords. At least I think
 	position = StartPos.get_origin()
 	show()
 	$CollisionShape2D.disabled = false
+	
+func Wait(sec):
+	var t = Timer.new()
+	t.set_wait_time(sec)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	t.queue_free()
+	pass
