55e30754d57ff5045ce75b084ff297b7d1650361
Broc <thebattlefrog@gmail.com>
2018-02-28 22:49:58 -0500

    Started Moving Camera
    Also buliding project with Godot 3.0.1

--- Scenes/ButtonPanel.gd
+++ Scenes/ButtonPanel.gd
@@ -7,6 +7,5 @@ func _on_PlayButton_pressed():
 	# TODO: Arcade Mode needs to be here!!
 	get_tree().change_scene("res://Scenes/StoryLevels/Level1.tscn")
 
-
 func _on_QuitButton_pressed():
 	get_tree().quit()
