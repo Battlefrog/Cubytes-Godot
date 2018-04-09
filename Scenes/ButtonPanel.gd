fe0e98d34acb44dd809135b66b49b98c2643545d
Broc <thebattlefrog@gmail.com>
2018-04-09 15:42:24 -0400

    Updated to 3.0.2
    - Added some Menu functionality (Options menu not in yet)
    
    (The collisions are still awful, I need to redo them.)

--- Scenes/ButtonPanel.gd
+++ Scenes/ButtonPanel.gd
@@ -1,11 +1,8 @@
 extends Panel
 
-func _ready():
-	pass
-
 func _on_PlayButton_pressed():
 	# TODO: Arcade Mode needs to be here!!
-	get_tree().change_scene("res://Scenes/StoryLevels/Level1.tscn")
+	get_tree().change_scene("res://Scenes/ModeSelection.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
