79a13a64978c3ddb0b807caa20d35462c1191e06
Broc <thebattlefrog@gmail.com>
2018-05-22 19:29:38 -0400

    Bombs no longer self-destruct
    This really doesn't make sense but in practice it works well.
    
    In polish I'll add an animation which makes it explode but another one pops out from the grid.

--- Scripts/Bomb.gd
+++ Scripts/Bomb.gd
@@ -5,9 +5,5 @@ var Exploded = false
 func _ready():
 	$ExplosionSFX.stream.loop = false
 	
-func Blowup():
-	if !Exploded:
-		$ExplosionSFX.play()
-		$Sprite.hide()
-		$CollisionShape2D.set_disabled(true)
-		Exploded = true
+func Blowup():	
+	$ExplosionSFX.play()
--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -1,8 +1,10 @@
 extends Label
 
 func _process(delta):
+	
+	#TODO: Dumb to do this every frame.
 	var d = ProjectSettings.get_setting("PLAYER_DEATHS")
 	
 	self.set_text("Deaths: " + str(d))
 	
-	print(str(d))
+	# print(str(d))
