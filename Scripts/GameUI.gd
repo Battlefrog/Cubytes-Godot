1d99e97c440ca32a470c52eb81e15a99ed86abcb
Broc <thebattlefrog@gmail.com>
2018-05-27 15:54:41 -0400

    Added FPS Display
    Also connected the death display to the signal

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -2,10 +2,16 @@ extends Node
 
 export (int) var CurrentLevelNum
 
+func _ready():
+	get_node("../Player").connect("player_death", self, "update_death_display")
+	
 func _process(delta):
+	print(Performance.get_monitor(Performance.TIME_FPS))
+	print(Engine.is_editor_hint())
 	
-	#TODO: Dumb to do this every frame.
-	var d = ProjectSettings.get_setting("PLAYER_DEATHS")
-	
-	$DeathDisplay.set_text("Deaths: " + str(d))
 	$LevelDisplay.set_text("Level: " + str(CurrentLevelNum))
+	
+	$FPSDisplay.set_text("FPS: " + str(Performance.get_monitor(Performance.TIME_FPS)))
+	
+func update_death_display(death):
+	$DeathDisplay.set_text("Deaths: " + str(death))
