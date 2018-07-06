bc8d7e9afe01a2edb647ddac712f94ac3066f9fd
Broc <Battlefrog@users.noreply.github.com>
2018-07-06 17:00:06 -0400

    Fixed Wrong Button States in Options Menu
    finally.

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready():
+	$Panel/VsyncToggle.pressed = OS.is_vsync_enabled()
+	$Panel/FullscreenToggle.pressed = OS.is_window_fullscreen()
+
 func _on_BackButton_pressed():
 	get_node("/root/SFXPlayer").play_sfx("SFXBack")
 	get_node("/root/global").goto_scene("res://Scenes/MainMenu.tscn")
