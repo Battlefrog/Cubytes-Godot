e2c7e8461e5b64dc35939bcff0a7e9d0255d0114
Broc <Battlefrog@users.noreply.github.com>
2018-08-09 15:40:19 -0400

    Fixed button not having a focused theme
    Also made the dialogue box have a bit of a delay, due to the GameUI not updating the level number

--- Scripts/BlockTutorial.gd
+++ Scripts/BlockTutorial.gd
@@ -7,4 +7,5 @@ export (String, MULTILINE) var ui_text
 export (Texture) var ui_picture_path
 
 func _ready():
+	yield(get_tree().create_timer(0.05), "timeout")
 	emit_signal("block_tutorial", ui_text, ui_picture_path, ui_name)
