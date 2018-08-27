05e34e9f685a90641cc609de8315ed279f7078d5
Broc <Battlefrog@users.noreply.github.com>
2018-08-27 16:56:33 -0400

    Hooked up Arcade Menu
    Just descriptions and titles.

--- Scripts/ArcadeMode.gd
+++ Scripts/ArcadeMode.gd
@@ -3,3 +3,8 @@ extends Control
 func _on_BackButton_pressed():
 	get_node("/root/SFXPlayer").play_sfx("SFXBack")
 	get_node("/root/global").goto_scene("res://Scenes/ModeSelection.tscn")
+
+
+func level_pressed(num, description):
+	$LevelDetails/LevelName.set_text("Level " + str(num))
+	$LevelDetails/LevelDescription.set_text(description)
