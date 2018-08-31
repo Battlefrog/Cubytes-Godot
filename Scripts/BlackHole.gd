b6e9b5709b69c430c4a1d174ed332c687c1b3897
Broc <Battlefrog@users.noreply.github.com>
2018-08-31 18:46:52 -0400

    Changed black hole mechanics
    If you touch the hole, you die.
    
    Also added Level 31

--- Scripts/BlackHole.gd
+++ Scripts/BlackHole.gd
@@ -1,8 +1,5 @@
 extends Area2D
 
-func _ready():
-	pass
-
 func _on_BlackHole_entered(body):
 	if body.is_in_group("Player"):
 		body.call("decrease_speed")
@@ -18,7 +15,14 @@ func _on_BlackHole_exited(body):
 func _on_Timer_timeout():
 	for body in get_overlapping_bodies():
 		if body.is_in_group("Player"):
-			$Timer.stop()
-			body.call("die")
-			get_node("/root/SFXPlayer").play_sfx("SFXShooterShoot")
-			$AudioStreamPlayer2D.stop()
+			kill_player(body)
+
+func _on_Area2D_entered(body):
+	if body.is_in_group("Player"):
+		kill_player(body)
+
+func kill_player(player):
+	$Timer.stop()
+	player.call("die")
+	get_node("/root/SFXPlayer").play_sfx("SFXShooterShoot")
+	$AudioStreamPlayer2D.stop()
