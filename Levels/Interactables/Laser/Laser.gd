d421f4ac76355ffd92215bc61e695cfa27a7ae7b
Broc <thebattlefrog@gmail.com>
2018-10-01 18:18:58 -0400

    Fixed laser's collision from breaking with multiple
    Need to tweak the collision size later

--- Levels/Interactables/Laser/Laser.gd
+++ Levels/Interactables/Laser/Laser.gd
@@ -6,13 +6,12 @@ const SEGMENT_LENGTH = 16
 export(DIRECTIONS) var laser_direction
 export(int) var laser_length
 
+var x_offset = 0
+var y_offset = 0
+var x_change = 0
+var y_change = 0
+
 func _ready():
-	var x_offset = 0
-	var y_offset = 0
-	
-	var x_change = 0
-	var y_change = 0
-	
 	# Setting offset based on direction
 	if laser_direction == DIRECTIONS.Left:
 		x_offset = -SEGMENT_LENGTH
@@ -29,17 +28,10 @@ func _ready():
 		x_change += x_offset
 		y_change += y_offset
 	
-	# Setting the collisions for the laser
-	var weight = 5
-	
-	if x_change == 0:
-		$LaserCollision/LaserCollisionShape.get_shape().set_a(Vector2(x_change, y_change - weight))
-	elif y_change == 0:
-		$LaserCollision/LaserCollisionShape.get_shape().set_a(Vector2(x_change - weight, y_change))
-	else:
-		$LaserCollision/LaserCollisionShape.get_shape().set_a(Vector2(x_change - weight, y_change - weight))
-	
-	
+	var shape = SegmentShape2D.new()
+	shape.set_a(Vector2(self.x_change, self.y_change))
+	shape.set_b(Vector2(0, 0))
+	$LaserCollision.shape_owner_add_shape($LaserCollision.create_shape_owner(self), shape)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
