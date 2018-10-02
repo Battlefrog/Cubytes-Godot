d60cfffe9db47d5e95ecf0ba0a52b338e460117a
Broc <thebattlefrog@gmail.com>
2018-10-02 06:50:18 -0400

    Added cheats on by default in the editor

--- Globals/global.gd
+++ Globals/global.gd
@@ -5,8 +5,14 @@ var audio_bus = preload("res://Misc/default_bus_layout.tres")
 
 func _ready():
 	ProjectSettings.set_setting("PLAYER_DEATHS", 0)
-	ProjectSettings.set_setting("debug_mode", false)
-	ProjectSettings.set_setting("click_teleport", false)
+	
+	if OS.is_debug_build():
+		ProjectSettings.set_setting("debug_mode", true)
+		ProjectSettings.set_setting("click_teleport", true)
+	else:
+		ProjectSettings.set_setting("debug_mode", false)
+		ProjectSettings.set_setting("click_teleport", false)
+		
 	ProjectSettings.set_setting("completed_story_mode", false)
 	
 	AudioServer.set_bus_layout(audio_bus)
@@ -16,18 +22,18 @@ func _ready():
 	current_scene = root.get_child(root.get_child_count() - 1)
 
 func goto_scene(path):
-    # Deleting the current scene at this point might be
-    # a bad idea, because it may be inside of a callback or function of it.
-    # The worst case will be a crash or unexpected behavior.
-
-    # The way around this is deferring the load to a later time, when
-    # it is ensured that no code from the current scene is running:
-    call_deferred("_deferred_goto_scene", path)
+	# Deleting the current scene at this point might be
+	# a bad idea, because it may be inside of a callback or function of it.
+	# The worst case will be a crash or unexpected behavior.
+	
+	# The way around this is deferring the load to a later time, when
+	# it is ensured that no code from the current scene is running:
+	 call_deferred("_deferred_goto_scene", path)
 
 func _deferred_goto_scene(path):
 	current_scene.free()
-
-    # Load new scene
+	
+	 # Load new scene
 	var s = ResourceLoader.load(path)
 	
 	current_scene = s.instance()
